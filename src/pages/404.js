fb131eb533e41d8c754bb5721d93e6b8b10ea538
TonyPepeBear <41405045+TonyPepeBear@users.noreply.github.com>
2022-03-03 11:30:34 +0000

    404 title

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { ReactComponent as LogoSvg } from "../images/svg/logo.svg";
+import { Helmet } from "react-helmet";
 
 const NotFoundPage = () => {
   return (
@@ -7,6 +8,9 @@ const NotFoundPage = () => {
       className="h-screen w-screen content-center items-center justify-center m-0"
       style={{ background: "#4d689b" }}
     >
+      <Helmet>
+        <title>iOS Club - 404</title>
+      </Helmet>
       <div className="h-full w-full flex flex-col justify-center items-center p-20">
         <div class="w-full flex flex-col md:flex-row items-center justify-center space-x-4 space-y-3">
           <LogoSvg className="h-48 w-48 md:w-32 md:h-32 text-white fill-current" />
