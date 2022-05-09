3440126687cc8fdc9378d548032965da46388a0c
TonyPepeBear <41405045+TonyPepeBear@users.noreply.github.com>
2022-05-09 10:19:27 +0000

    words aligin

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,17 +12,17 @@ const IndexPage = () => {
       {/* Header */}
       <AppHeader />
       {Navbar()}
-      <div className="container mx-auto break-all bg-white shadow-lg">
+      <div className="container mx-auto break-all bg-white shadow-lg px-3 md:px-0">
         {/* 空白區 */}
         <div className="h-0 md:h-32"></div>
         {/* We are iOS Club */}
         <div className="py-5 md:flex md:p-10 md:space-x-10 justify-center">
           <div className=" md:w-4/12 w-full self-center">
             <div className="text-center text-5xl font-bold">
-              <h1 className="py-3">We are</h1>
-              <h1 className="py-3">iOS Club.</h1>
+              <h1 className="py-3 text-center md:text-left">We are</h1>
+              <h1 className="py-3 text-center md:text-left">iOS Club.</h1>
             </div>
-            <p className="py-3 text-center text-gray-700">
+            <p className="py-3 text-gray-700 text-justify">
               一個教你從零開始打造專屬 apps 的社團
             </p>
           </div>
@@ -37,7 +37,7 @@ const IndexPage = () => {
               <h1 className="py-3">About</h1>
               <h1 className="py-3">iOS Club.</h1>
             </div>
-            <p className="py-3 text-center text-gray-700">
+            <p className="py-3 text-gray-700 text-justify">
               是全台唯二 Apple
               官方認證的社團，也是一個結合學術的志工性社團。每年寒、暑假社員們會有國際交流的機會，到海外參加
               iOS Club 官方舉辦的冬、夏令營，
@@ -52,7 +52,7 @@ const IndexPage = () => {
             <div className="text-center text-5xl font-bold">
               <h1 className="py-3">iOS Club 會做什麼</h1>
             </div>
-            <p className="py-3 text-center text-gray-700">
+            <p className="py-3 text-gray-700 text-justify">
               在比賽方面， 社團鼓勵所有社員參加對外性比賽。 近年來也都在WWDC
               學生挑戰賽及移動應用創新賽等比賽取得佳績。
               當然，平時社團活動也不僅僅有社課，我們還會舉辦講座、夜烤、WorkShop、社遊、期末聚等，超多活動等你們來參加！
