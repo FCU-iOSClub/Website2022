e074c22b216b4ee4fca9c5df6933bfd38b26e318
TonyPepeBear <41405045+TonyPepeBear@users.noreply.github.com>
2022-05-09 06:37:43 +0000

    gallery template

--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -1,13 +1,30 @@
 import * as React from "react";
+import Footer from "../components/footer";
+import AppHeader from "../components/header";
+import Navbar from "../components/navbar";
 
 const Gallery = (props) => {
   const context = props.pageContext;
   return (
-    <div>
-      {context.name}
-      {context.photos.map((item, index) => (
-        <div>{item}</div>
-      ))}
+    <div className="bg-blue-300">
+      <AppHeader title={context.name} />
+      {Navbar()}
+      <div className="container mx-auto break-all bg-white">
+        <h1 className="pt-12 px-1 text-3xl w-full text-center font-bold">
+          {context.name}
+        </h1>
+        <p className="py-2 text-center w-full text-red-400 font-bold">
+          {context.date}
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-4 p-4 gap-4">
+          {context.photos.map((item, index) => (
+            <a href={item}>
+              <img src={item} />
+            </a>
+          ))}
+        </div>
+      </div>
+      {Footer()}
     </div>
   );
 };
