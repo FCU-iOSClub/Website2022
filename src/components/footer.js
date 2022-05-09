cc63539ac284430719636e2b4e563b033a1dc189
TonyPepeBear <41405045+TonyPepeBear@users.noreply.github.com>
2022-05-09 06:01:09 +0000

    footer gallery list href

--- src/components/footer.js
+++ src/components/footer.js
@@ -42,7 +42,7 @@ const Footer = () => {
           <a href="" className="text-gray-600">
             行事曆
           </a>
-          <a href="" className="text-gray-600">
+          <a href="/gallery_list" className="text-gray-600">
             活動相簿
           </a>
           <a href="" className="text-gray-600">
