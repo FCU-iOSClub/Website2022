504b76c2463e8f5eeddaa18ebd150329936dec77
TonyPepeBear <41405045+TonyPepeBear@users.noreply.github.com>
2022-05-13 14:36:51 +0000

    404 back to home

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,21 @@ import { ReactComponent as LogoSvg } from "../images/svg/logo.svg";
 import AppHeader from "../components/header";
 
 const NotFoundPage = () => {
+  const [countDown, setCountDown] = React.useState(5);
+  // 開始倒數
+  React.useEffect(() => {
+    let id = setInterval(() => {
+      setCountDown(countDown - 1);
+    }, 1000);
+  });
+
+  // 倒數結束回首頁
+  React.useEffect(() => {
+    if (countDown === 0) {
+      window.location.href = "/";
+    }
+  });
+
   return (
     <div
       className="h-screen w-screen content-center items-center justify-center m-0"
@@ -14,6 +29,7 @@ const NotFoundPage = () => {
           <LogoSvg className="h-48 w-48 md:w-32 md:h-32 text-white fill-current" />
           <h2 className="text-white text-xl md:text-3xl">網頁努力建置中...</h2>
         </div>
+        <p className="text-white">在 {countDown} 秒後幫您回首頁</p>
 
         <div className="mt-10">
           <h2 className="text-lg text-white">404 Not Found</h2>
