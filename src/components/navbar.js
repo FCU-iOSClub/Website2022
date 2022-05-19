b1b8947c2632ac3e5f743b2f8b890b7424ed0c84
TonyPepeBear <41405045+TonyPepeBear@users.noreply.github.com>
2022-05-19 17:11:05 +0000

    new navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,21 +2,41 @@ import * as React from "react";
 import { Icon } from "@iconify/react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  withWidth();
+
   const mobileChild = (
-    <nav className="flex justify-between fixed px-6 py-3 bg-gray-100 w-full font-bold shadow-md z-50">
-      <a href="/" className="w-fit text-2xl">
-        iOS Club
-      </a>
-      <div className="flex gap-2">
-        {menuItems.map((item, index) => (
-          <div
-            className="p-2 bg-iosbgblue rounded-xl hover:bg-blue-200"
-            onClick={() => (location.href = item.url)}
+    <nav className="fixed w-full h-fit bg-gray-100  font-bold shadow-md z-50">
+      <div className="flex justify-between px-6 py-3 w-full">
+        <a href="/" className="w-fit text-2xl">
+          iOS Club
+        </a>
+        <div className="place-self-center">
+          <button
+            className="flex items-center focus:outline-none"
+            onClick={() => setIsOpen(!isOpen)}
           >
-            <Icon icon={item.icon} color="#4f7096" />
-          </div>
-        ))}
+            <Icon
+              icon={isOpen ? "akar-icons:cross" : "bx:menu"}
+              className="w-6 h-6"
+            />
+          </button>
+        </div>
       </div>
+      {isOpen && (
+        <ul className="md:px-24 lg:px-60 xl:px-80 flex flex-col justify-between gap-3 pl-5 py-3">
+          {menuItems.map((item, index) => (
+            <li key={index}>
+              <a
+                href={item.url}
+                className="hover:underline underline-offset-8 w-full"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
   const desktopChild = (
