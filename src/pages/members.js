fc96abb3603da68de578c7a48bd85f42ddbdb216
FOR-Shu <shuyuanchuang2020@gmail.com>
2022-05-27 05:31:32 +0800

    update

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,13 +1,8 @@
 import * as React from "react";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
-import { ReactComponent as LogoSvg } from "../images/svg/logo.svg";
-import { ReactComponent as AboutIosSvg } from "../images/svg/about_ios.svg";
-import { ReactComponent as IosClubDoingSvg } from "../images/svg/iosclub_doing.svg";
 import AppHeader from "../components/header";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
-import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
 
 const MembersPage = () => {
   return (
