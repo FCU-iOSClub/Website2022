29c4e61ae38171ade4d44574a51fd8680adf3dc1
FOR-Shu <shuyuanchuang2020@gmail.com>
2022-05-27 05:36:24 +0800

    update 'member.js' - title

--- src/pages/members.js
+++ src/pages/members.js
@@ -11,7 +11,20 @@ const MembersPage = () => {
       <AppHeader />
       <Navbar />
       {/* body */}
-      <div className="container mx-auto break-all bg-white shadow-lg px-3 md:px-0 font-serif"></div>
+      <div className="container mx-auto break-all bg-white shadow-lg px-3 md:px-0 font-serif">
+        <div className="h-20 md:h-32" /> {/* 空白 */}
+        {/* Title */}
+        <div className="py-5 md:px-32 grid grid-rows-1 md:grid-cols-2 md:p-10 md:space-x-10 justify-center items-center">
+          <div className="w-full self-center">
+            <div className="text-center text-5xl font-bold">
+              <h1 className="py-3 text-center md:text-left">歷代社團幹部</h1>
+            </div>
+            <p className="py-3 text-gray-700 text-center md:text-left">
+              感謝各位幹部的辛苦付出。
+            </p>
+          </div>
+        </div>
+      </div>
       {/* footer */}
       <Footer />
     </div>
