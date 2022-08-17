0e0e6e338712ba9cee71f3950b21a6d9d4f69064
TonyPepe <tonybear55665566@gmail.com>
2022-08-17 21:50:45 +0800

    新版競賽 Fixes #42

--- src/pages/contest.js
+++ src/pages/contest.js
@@ -13,54 +13,27 @@ const ContestPage = () => {
         <div className="h-32" /> {/* 空白 */}
         <h1 className="text-5xl text-center font-bold">iOS Club 競賽</h1>
         <div className="h-16" /> {/* 空白 */}
-        <div>
-          {" "}
-          {/* WWDC */}
-          <div className="w-full bg-ioscardblue py-3">
-            {" "}
-            {/* WWDC title */}
-            {/* oneline */}
-            <h2 className="hidden md:block text-xl text-center text-white font-bold">
-              WWDC 全球學生挑戰賽 (Swift Student Challenge)
-            </h2>
-            {/* two lines */}
-            <h2 className="block md:hidden text-xl text-center text-white font-bold">
-              WWDC 全球學生挑戰賽
-              <br />
-              (Swift Student Challenge)
-            </h2>
-          </div>{" "}
-          {/* WWDC end title */}
-          {wwdcData.map((item) => (
-            <div className="border-b w-full pt-8 pb-1 grid grid-rows-1 md:grid-cols-3 text-iostextblue font-medium gap-2">
-              <p className="px-3 text-left md:text-center">{item[0]}</p>
-              <p className="px-3 text-left md:text-center">{item[1]}</p>
-              <p className="px-3 text-left">{item[2]}</p>
+        {contestData.map((item, index) => {
+          return (
+            <div className="py-6">
+              {/* title */}
+              <div className="w-full bg-ioscardblue py-3">
+                <h2 className="text-xl text-center text-white font-bold">
+                  {item.title}
+                </h2>
+              </div>
+              {/* WWDC end title */}
+              {item.contents.map((item) => (
+                <div className="border-b w-full pt-8 pb-1 grid grid-rows-1 md:grid-cols-4 text-iostextblue font-medium gap-2">
+                  <p className="px-3 text-left md:text-center">{item[0]}</p>
+                  <p className="px-3 text-left md:text-center">{item[1]}</p>
+                  <p className="px-3 text-left md:text-center">{item[2]}</p>
+                  <p className="px-3 text-left md:text-center">{item[3]}</p>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        {/* END WWDC */}
-        <div className="h-16" /> {/* 空白 */}
-        <div>
-          {" "}
-          {/* 移動應用 */}
-          <div className="w-full bg-ioscardblue py-3">
-            {" "}
-            {/* WWDC */}
-            <h2 className="text-xl text-center text-white font-bold">
-              APP 移動應用創新賽
-            </h2>
-          </div>{" "}
-          {/* WWDC end title */}
-          {appCreativeData.map((item) => (
-            <div className="border-b w-full pt-8 pb-1 grid grid-rows-1 md:grid-cols-3 text-iostextblue font-medium gap-2">
-              <p className="px-3 text-left md:text-center">{item[0]}</p>
-              <p className="px-3 text-left md:text-center">{item[1]}</p>
-              <p className="px-3 text-left">{item[2]}</p>
-            </div>
-          ))}
-        </div>
-        {/* 移動應用 */}
+          );
+        })}
         <div className="h-16" /> {/* 空白 */}
       </div>
       {/* footer */}
@@ -69,29 +42,105 @@ const ContestPage = () => {
   );
 };
 
-const wwdcData = [
-  ["2022 WWDC 得獎", "魯敬元 學長", "作品《MusicLearning》"],
-  ["2021 WWDC 得獎", "薛竣祐 學長", "作品《Hello World》"],
-  ["2020 WWDC 得獎", "薛竣祐 學長", "作品《Camel Playground》"],
-  ["2020 WWDC 得獎", "劉祐炘 學長", "作品《Gi的故事》"],
-];
-
-const appCreativeData = [
-  ["2021 大中華區三等獎", "黃子騰、温子瑩、余睿霖", "作品《觸幕可見》"],
-  ["2021 台灣區二等獎", "黃子騰、温子瑩、余睿霖", "作品《觸幕可見》"],
-  ["2021 台灣區佳作", "林祺鈞、張宸、許友齊", " 作品《Magic Drums》"],
-  ["2020 大中華區三等獎", "薛竣祐、黃傳霖、魯敬元", "作品《MC 逆滲透》"],
-  ["2020 台灣區二等獎", "薛竣祐、黃傳霖、魯敬元", "作品《MC 逆滲透》"],
-  [
-    "2019 大中華區一等獎",
-    "黃郁洺、霍湛軒、曾昱翔",
-    "作品《Help.Heal.Healthy》",
-  ],
-  ["2019 台灣二等獎", "黃郁洺、霍湛軒、曾昱翔", "作品《Help.Heal.Healthy》"],
-  ["2018 大中華區三等獎", "蔡昌銘、賴亨學、劉祐炘", "作品《守護燈塔》"],
-  ["2018 台灣區三等獎", "蔡昌銘、賴亨學、劉祐炘", "作品《守護燈塔》"],
-  ["2017 大中華區三等獎", "阮揚洲、陳薇涵、荊輔翔", "作品《天氣即時預報》"],
-  ["2017 台灣區特獎", "阮揚洲、陳薇涵、荊輔翔", "作品《天氣即時預報》"],
+const contestData = [
+  {
+    title: "2022 社團成員競賽得獎名單",
+    contents: [["WWDC 全球學生挑戰賽", "獲獎", "魯敬元", "《MusicLearning》"]],
+  },
+  {
+    title: "2021 社團成員競賽得獎名單",
+    contents: [
+      ["WWDC 全球學生挑戰賽", "獲獎", "薛竣祐", "《Hello World》"],
+      [
+        "App 移動應用創新賽",
+        "大中華區三等獎",
+        "黃子騰、温子瑩、余睿霖",
+        "《觸幕可見》",
+      ],
+      [
+        "App 移動應用創新賽",
+        "台灣區二等獎",
+        "黃子騰、温子瑩、余睿霖",
+        "《觸幕可見》",
+      ],
+      [
+        "App 移動應用創新賽",
+        "台灣區佳作",
+        "林祺鈞、張宸、許友齊",
+        "《Magic Drums》",
+      ],
+    ],
+  },
+  {
+    title: "2020 社團成員競賽得獎名單",
+    contents: [
+      ["WWDC 全球學生挑戰賽", "獲獎", "薛竣祐", "《Camel Playground》"],
+      ["WWDC 全球學生挑戰賽", "獲獎", "劉祐炘", "《Gi的故事》"],
+      [
+        "App 移動應用創新賽",
+        "大中華區三等獎",
+        "薛竣祐、黃傳霖、魯敬元",
+        "《MC 逆滲透》",
+      ],
+      [
+        "App 移動應用創新賽",
+        "台灣區二等獎",
+        "薛竣祐、黃傳霖、魯敬元",
+        "《MC 逆滲透》",
+      ],
+    ],
+  },
+  {
+    title: "2019 社團成員競賽得獎名單",
+    contents: [
+      [
+        "App 移動應用創新賽",
+        "大中華區一等獎",
+        "黃郁洺、霍湛軒、曾昱翔",
+        "《Help.Heal.Healthy》",
+      ],
+      [
+        "App 移動應用創新賽",
+        "台灣區二等獎",
+        "黃郁洺、霍湛軒、曾昱翔",
+        "《Help.Heal.Healthy》",
+      ],
+    ],
+  },
+  {
+    title: "2018 社團成員競賽得獎名單",
+    contents: [
+      [
+        "App 移動應用創新賽",
+        "大中華區三等獎",
+        "蔡昌銘、賴亨學、劉祐炘",
+        "《守護燈塔》",
+      ],
+      [
+        "App 移動應用創新賽",
+        "台灣區三等獎",
+        "蔡昌銘、賴亨學、劉祐炘",
+        "《守護燈塔》",
+      ],
+    ],
+  },
+  {
+    title: "2017 社團成員競賽得獎名單",
+    contents: [
+      [
+        "App 移動應用創新賽",
+        "大中華區特獎",
+        "阮揚州、陳薇涵、荊",
+        "《天氣即時預報》",
+      ],
+      [
+        "App 移動應用創新賽",
+        "台灣區三等獎",
+        "阮揚州、陳薇涵、荊",
+        "《天氣即時預報》",
+      ],
+    ],
+  },
 ];
 
 export default ContestPage;
