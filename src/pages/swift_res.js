bd1447fab7a20750680502241f705a9bf4c9bd09
kr1s0404 <tylerlolz0404@gmail.com>
2022-10-05 17:51:13 +0800

    add section2 in swift_res

--- src/pages/swift_res.js
+++ src/pages/swift_res.js
@@ -14,7 +14,9 @@ const SwiftPage = () => {
       <div className="container font-serif mx-auto break-all bg-white shadow-lg px-3 md:px-0 text-justify">
         {/* 空白區 */}
         <div className="h-32" />
-        <div className="mx-12 flex flex-col lg:flex-row py-8 px-4 md:px-12 border border-gray-400 rounded-3xl shadow-2xl gap-7 items-center">
+
+        {/* 第一個section */}
+        <div className="mx-12 flex flex-col lg:flex-row py-8 px-4 md:px-16 border border-gray-400 rounded-3xl shadow-2xl gap-7 items-center">
           <div className="flex flex-col gap-4 xl:w-5/6">
             <h2 className="text-3xl font-bold break-normal">
               Develop in Swift
@@ -50,6 +52,46 @@ const SwiftPage = () => {
             <div></div>
           </div>
         </div>
+
+        {/* 空白區 */}
+        <div className="h-16" />
+
+        {/* 第二個section */}
+        <div className="mx-12 flex flex-col lg:flex-row py-8 px-4 md:px-16 border border-gray-400 rounded-3xl shadow-2xl gap-7 items-center">
+          <div className="flex flex-col gap-4 ">
+            <h2 className="text-3xl font-bold break-normal text-center">
+              Develop in Swift curriculum
+            </h2>
+            <p className="py-10 break-normal">
+              Get started or take your skills to the next level. The Develop in
+              Swift books are flexible enough to help you whether you’re new to
+              coding or want to advance your skills. These books provide
+              practical experience in creating apps on Mac using Xcode, the
+              integrated development environment used to build apps for Apple
+              platforms.
+            </p>
+
+            <div className=" flex flex-col md:flex-row gap-24">
+              {swiftBooks.map((item, index) => (
+                <div className=" flex-1 flex-col">
+                  <img className="object-contain " src={item.image} />
+                  <p className="break-normal text-2xl font-black text-left py-8">
+                    {item.title}
+                  </p>
+
+                  <p className="break-left py-4">{item.desc}</p>
+
+                  <a href={item.url} className="text-blue-600 ">
+                    View in Apple Books &gt;
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* 空白區 */}
+        <div className="h-16" />
       </div>
       {/* footer */}
       <Footer />
@@ -81,3 +123,27 @@ const courseCard = (title, img) => {
 };
 
 export default SwiftPage;
+
+const swiftBooks = [
+  {
+    title: "Develop in Swift Explorations",
+    desc: "Learn key computing concepts, building a solid foundation in programming with Swift. And, learn about the impact of computing and apps on society, economies, and cultures while exploring iOS app development.",
+    url: "https://apple.co/teachingcode",
+    image:
+      "https://github.com/FCU-iOSClub/Website2022ImageBed/blob/main/swift-data-collections_2x.jpg?raw=true",
+  },
+  {
+    title: "Develop in Swift Fundamentals",
+    desc: "Build fundamental iOS app development skills with Swift. And, master the core concepts and practices that Swift programmers use daily and build a basic fluency in Xcode’s source and UI editors.",
+    url: "https://apple.co/teachingcode",
+    image:
+      "https://github.com/FCU-iOSClub/Website2022ImageBed/blob/main/swift-explorations_2x.jpg?raw=true",
+  },
+  {
+    title: "Develop in Swift Data Collections",
+    desc: "Extend your knowledge and skill in iOS app development creating more complex and capable apps. And, work with data from a server and explore new iOS APIs that allow for much richer app experiences — including displaying large collections of data in multiple formats.",
+    url: "https://apple.co/teachingcode",
+    image:
+      "https://github.com/FCU-iOSClub/Website2022ImageBed/blob/main/swift-fundamentals_2x.jpg?raw=true",
+  },
+];
