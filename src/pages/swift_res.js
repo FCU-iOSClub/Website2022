25ff2d8c5473c9a8caae5c78bd13fd91ec612456
kr1s0404 <tylerlolz0404@gmail.com>
2023-02-16 14:22:48 +0800

    Update image URL

--- src/pages/swift_res.js
+++ src/pages/swift_res.js
@@ -147,11 +147,11 @@ const SwiftPage = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 px-3 sm:px-10 md:px-32 lg:px-48 xl:px-64">
               <img
                 className="w-full h-full object-contain"
-                src="https://raw.githubusercontent.com/FCU-iOSClub/Website2022ImageBed/main/Swift%20Associate%20Badge.png"
+                src="https://raw.githubusercontent.com/FCU-iOSClub/Website2022ImageBed/main/App_Development_Swift_CU_Badge_020121.jpeg"
               />
               <img
                 className="w-full object-contain"
-                src="https://raw.githubusercontent.com/FCU-iOSClub/Website2022ImageBed/main/Certified%20User%20Badge.png"
+                src="https://raw.githubusercontent.com/FCU-iOSClub/Website2022ImageBed/main/App_Development_Swift_Associate_Badge_020121.jpeg"
               />
             </div>
           </div>
