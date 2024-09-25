53fb273c3f6c1d36caab1e1b3a8c11a3ace46fe3
TonyPepe <tonybear55665566@gmail.com>
2024-09-25 22:10:38 +0800

    add threads

--- src/components/footer.js
+++ src/components/footer.js
@@ -18,6 +18,11 @@ const communityList = [
     href: "https://www.instagram.com/fcu.iosclub/",
   },
   {
+    name: "Threads",
+    icon: "akar-icons:threads-fill",
+    href: "https://www.threads.net/@fcu.iosclub",
+  },
+  {
     name: "Line 社群",
     icon: "bi:line",
     href: "https://line.me/ti/g2/Hcrx-jxTUDGItqDEo0R63w?utm_campaign=default&utm_medium=link_copy&utm_source=invitation",
