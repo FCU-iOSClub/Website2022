d7b034893938d8fa030ae6325b5b4aca48fd18f7
Qing Bi Liao <78472206+Pencil126@users.noreply.github.com>
2025-09-23 04:37:24 +0000

    Update 社課連結

--- src/pages/course.js
+++ src/pages/course.js
@@ -10,7 +10,7 @@ const CoursePage = () => {
   useGoogleAdsConversion();
 
   const hideURL =
-    "https://drive.google.com/drive/folders/1tdW1rLbbGZH0Z-ipcXY_fuhn1kLtHnIn?usp=share_link";
+    "https://drive.google.com/drive/folders/1bn2eXfrgEQ6DJY4RiYqoMla9nmEmmeAp?usp=drive_link";
 
   return (
     <div className=" bg-iosbgblue">
