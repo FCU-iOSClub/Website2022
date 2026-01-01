0d40d766987edcb9ce6ef1fac00361a3a865207e
Qing Bi Liao <78472206+Pencil126@users.noreply.github.com>
2026-01-01 10:50:08 +0000

    Copyright -2026.

--- src/components/footer.js
+++ src/components/footer.js
@@ -86,7 +86,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="pt-12 text-gray-100 text-center flex flex-col md:flex-row justify-center items-center">
-        <p>iOS Club 2016 - 2025</p>
+        <p>iOS Club 2016 - 2026</p>
         <p className="px-2 py-3">©</p>
         <p>All right reserved</p>
       </div>
