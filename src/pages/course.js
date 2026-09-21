4452709c73a8703132b35c53fdb25b497f99624e
TeaLance <LanceLeeGithub@gmail.com>
2026-09-21 13:26:41 +0000

    chore:更新社課簡報 Google Drive連結

--- src/pages/course.js
+++ src/pages/course.js
@@ -10,7 +10,7 @@ const CoursePage = () => {
   useGoogleAdsConversion();
 
   const hideURL =
-    "https://drive.google.com/drive/folders/1yapFEpfpyPIq_vieIbwBN8oJPu_K5YBp?usp=drive_link";
+    "https://drive.google.com/drive/folders/1SZIG6qQlnJvdlDzf8JhxC8W_9gjR30sx?usp=sharing"; /* 社課簡報連結*/
 
   return (
     <div className=" bg-iosbgblue">
